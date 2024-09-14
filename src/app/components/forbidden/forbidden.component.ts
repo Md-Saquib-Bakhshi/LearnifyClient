6e9a0bf1f8512f2a63dbd29be56a9b8849e6a25e
mdsaquib.bakhshi <mdsaquib.bakhshi@ust.com>
2024-09-14 10:44:57 +0530

    Add style for forbidded page

--- src/app/components/forbidden/forbidden.component.ts
+++ src/app/components/forbidden/forbidden.component.ts
@@ -1,24 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forbidden',
   templateUrl: './forbidden.component.html',
-  styleUrl: './forbidden.component.css'
+  styleUrls: ['./forbidden.component.css']
 })
-export class ForbiddenComponent {
+export class ForbiddenComponent implements OnInit, OnDestroy {
+  showErrorPage = true;
+  redirectTimeout: any;
+  errorPageTimeout: any;
+
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    setTimeout(() => {
-      const role = localStorage.getItem('userRole');
-      if (role?.includes('Admin')) {
-        this.router.navigate(['/admin']);
-      } else if (role?.includes('Student')) {
-        this.router.navigate(['/student']);
-      } else {
-        this.router.navigate(['/login']); 
-      }
-    }, 3000);
+    this.errorPageTimeout = setTimeout(() => {
+      this.showErrorPage = false; 
+      this.redirectTimeout = setTimeout(() => {
+        const role = localStorage.getItem('userRole');
+        if (role?.includes('Admin')) {
+          this.router.navigate(['/admin']);
+        } else if (role?.includes('Student')) {
+          this.router.navigate(['/student']);
+        } else {
+          this.router.navigate(['/login']); 
+        }
+      }, 3000);
+    }, 3000); 
+  }
+
+  ngOnDestroy(): void {
+    if (this.redirectTimeout) {
+      clearTimeout(this.redirectTimeout);
+    }
+    if (this.errorPageTimeout) {
+      clearTimeout(this.errorPageTimeout);
+    }
   }
 }

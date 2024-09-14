c9e53e346e9b0b871c120045052cc3598421fa88
mdsaquib.bakhshi <mdsaquib.bakhshi@ust.com>
2024-09-14 15:47:35 +0530

    Add profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { ProfileService } from '../../services/profile/profile.service';
+import { profileModel } from '../../models/profileModel';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
-  
+export class ProfileComponent implements OnInit {
+  userProfile!: profileModel;
+
+  constructor(private profileService: ProfileService, private toastr: ToastrService) {}
+
+  ngOnInit(): void {
+    this.loadProfile();
+  }
+
+  loadProfile(): void {
+    this.profileService.getProfile().subscribe({
+      next: (response) => {
+        if (response.status === 'Success') {
+          this.userProfile = response.data;
+          console.log('Profile Data:', this.userProfile);
+        } else {
+          this.toastr.error(response.message, 'Error');
+        }
+      },
+      error: (error) => {
+        console.error('Error fetching profile data:', error);
+        this.toastr.error('Error fetching profile data', 'Error');
+      }
+    });
+  }
+
+  updateProfile(): void {
+    if (this.userProfile) {
+      this.profileService.updateProfile(this.userProfile).subscribe({
+        next: (response) => {
+          if (response.status === 'Success') {
+            console.log('Profile updated successfully:', response);
+            this.toastr.success('Profile updated successfully', 'Success');
+            this.loadProfile();
+          } else {
+            this.toastr.error(response.message, 'Error');
+          }
+        },
+        error: (error) => {
+          console.error('Error updating profile:', error);
+          this.toastr.error('Error updating profile', 'Error');
+        }
+      });
+    }
+  }
 }
---
+++ src/app/services/profile/profile.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { profileModel } from '../../models/profileModel';
+import {jwtDecode} from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+  baseUrl = 'http://localhost:5000/api/user';
+
+  email: string | null = null;
+  role: string | null = null;
+
+  constructor(private http: HttpClient) {
+    this.email = this.getEmailFromToken();
+    this.role = localStorage.getItem('userRole');
+    console.log('Email from token:', this.email);
+    console.log('Role from local storage:', this.role);
+  }
+
+  getEmailFromToken(): string | null {
+    const token = localStorage.getItem('authenticationToken');
+    if (token) {
+      const decodedToken: any = jwtDecode(token);
+      console.log('Decoded Token:', decodedToken);
+      return decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"] || null;
+    }
+    return null;
+  }
+
+  
+
+  getProfile(): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/role/${this.role}/email/${this.email}`);
+  }
+
+  updateProfile(profile: profileModel): Observable<any> {
+    return this.http.put<any>(`${this.baseUrl}/role/${this.role}/email/${this.email}`, profile);
+  }
+}
